6c9daa96c31f9c4227d7604840769021824d7292
Poran chowdury <chowdury659@gmail.com>
2020-10-11 21:52:21 +0600

    web service info controller

--- src/main/java/com/example/demo/controller/SwaggerInitialController.java
+++ src/main/java/com/example/demo/controller/SwaggerInitialController.java
@@ -1,12 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
-@RequestMapping("/swagger")
 public class SwaggerInitialController {
     @GetMapping("/swagger")
     public RedirectView getSwager(){
