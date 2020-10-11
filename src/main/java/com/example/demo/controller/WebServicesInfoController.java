6aa709def11370076b61d8671ece316e7af49ab5
Poran chowdury <chowdury659@gmail.com>
2020-10-11 21:20:32 +0600

    web service info controller

--- src/main/java/com/example/demo/controller/WebServicesInfoController.java
+++ src/main/java/com/example/demo/controller/WebServicesInfoController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.WebServiceInfo;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +12,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/")
 public class WebServicesInfoController {
       @GetMapping("/")
-      public ResponseEntity<Object> getWebServicesInfo(){
+      public WebServiceInfo getWebServicesInfo(){
           WebServiceInfo webServiceInfo = new WebServiceInfo();
           webServiceInfo.setWebserviceName("Book Api");
           webServiceInfo.setVersion("api/v1");
           webServiceInfo.setName("MD Najmul Hasan");
           webServiceInfo.setId(2017100000028L);
           webServiceInfo.add(linkTo(methodOn(SwaggerInitialController.class)).withRel("swagger"));
-          return ResponseEntity.ok(webServiceInfo);
+          return webServiceInfo;
       }
 }
