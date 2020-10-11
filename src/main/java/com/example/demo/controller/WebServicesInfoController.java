7e72d01bba684c43991954ce3fa41356a920e407
Poran chowdury <chowdury659@gmail.com>
2020-10-11 20:58:52 +0600

    web service info controller

--- src/main/java/com/example/demo/controller/WebServicesInfoController.java
+++ src/main/java/com/example/demo/controller/WebServicesInfoController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.WebServiceInfo;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequestMapping("/")
 public class WebServicesInfoController {
+      @GetMapping
       public ResponseEntity<Object> getWebServicesInfo(){
           WebServiceInfo webServiceInfo = new WebServiceInfo();
           webServiceInfo.setWebserviceName("Book Api");
